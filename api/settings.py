92c6315f48daeb8df6d92af33c90ae98c0c332f8
Rakesh Ramchandra Zore <91056747+ZoreRakesh@users.noreply.github.com>
2023-04-02 08:35:16 +0530

    Delete api directory

--- api/settings.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import os
-
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-STATIC_URL = 'static/'
-STATICFILES_DIRS =os.path.join(BASE_DIR,'static')
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
